590c91322cf8954bcaea6eb90a1bea7fa35c5687
Martin Jordanov <xxm0703@abv.bg>
2019-03-01 17:34:14 +0200

    Some progress on logging and registration

--- server/api/server.rb
+++ server/api/server.rb
@@ -10,57 +10,73 @@ current_dir = File.dirname(__FILE__)
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
 ActiveRecord::Base.establish_connection(
-  { :adapter => 'postgresql',
-    :database => 'development',
-    :host => 'localhost',
-    :username => 'postgres',
-    :password => "postgres" }
-  )
+ adapter: 'postgresql',
+ database: 'development',
+ host: 'localhost',
+ username: 'postgres',
+ password: 'postgres'
+)
 
 enable :sessions
-connections = {}
+@@connections = {}
 
-s_rd_pipe, a_wr_pipe = IO.pipe
-a_rd_pipe, s_wr_pipe = IO.pipe
-`#{File.join(current_dir, '/../bin/')}start_game #{s_rd_pipe} #{s_wr_pipe}`
-s_rd_pipe.close
-s_wr_pipe.close
+before do
+  s_rd_pipe, a_wr_pipe = IO.pipe
+  a_rd_pipe, s_wr_pipe = IO.pipe
+  `#{File.join(current_dir, '/../bin/')}start_game #{s_rd_pipe} #{s_wr_pipe}`
+  s_rd_pipe.close
+  s_wr_pipe.close
+
+  content_type :json
+end
 
 helpers do
-  def generate_token(user)
-    (user.username + user.password).hash
-  end
+ def generate_token(user)
+  (user.username + user.password).hash
+ end
+
+ def log(user)
+   session['token'] = generate_token(user)
+   @@connections[session['token']] = user.id
+   {'logged': user.username}.to_json
+ end
 end
 
-#TODO permit only some params
+# TODO: permit only some params
 post '/register' do
-  begin
-    user = User.new(params)
-    if user.save!
-      status 201
-      session["token"] = generate_token(user)
-    end
-  # rescue Exception
-  #   status 422
+ begin
+  user = User.new(params)
+  if user.save!
+   status 201
+   log(user)
   end
-
+ rescue ActiveRecord::RecordNotUnique
+  status 422
+  {'error': 'username already taken'}.to_json
+ rescue Exception
+  status 400
+  {'error': 'Something went wrong with your registration'}.to_json
+ end
 end
 
-get '/new' do
-  token = generate_token(request.ip)
-  connections[token] = request.ip
-  "#{generate_token(request.ip)}  #{session['counter']}"
+post '/log' do
+  user = User.find_by username: params[:username]
+  if user.nil? == true or user.password != params[:password]
+    {'error': "Username or password, not currect"}.to_json
+  else
+    log(user)
+  end
 end
 
 get '/join_table' do
-	d = Deck.new()
-	"#{d.deal.to_s}  #{Random.rand}"
+ d = Deck.new
+ "#{d.deal}  #{Random.rand}"
 end
 
 get '/' do
-  @link = generate_token(request.ip).to_s
-  session['counter'] ||= 0
-  session['counter'] += 1
-  @counter = session['counter']
-  haml :index
+ @link = generate_token(request.ip).to_s
+ session['counter'] ||= 0
+ session['counter'] += 1
+ @counter = session['counter']
+ @@connections.to_json
 end
