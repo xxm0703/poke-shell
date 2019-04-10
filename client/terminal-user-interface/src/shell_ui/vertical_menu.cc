1e8266d55779a939f67bd7e37f2bb15c5fb623d0
Petar Nikolov <penikolov23@gmail.com>
2019-04-10 23:55:38 +0300

    [shell-ui] Implement VerticalMenu class.
    The unit tests for VerticalMenu have been altered slightly as the passed
    coordinates in the 'VerticalMenu::mvwprint()' method are not utilized.
    They may be used in the future.

--- client/terminal-user-interface/src/shell_ui/vertical_menu.cc
+++ client/terminal-user-interface/src/shell_ui/vertical_menu.cc
@@ -1,11 +1,20 @@
 #include "shell_ui/vertical_menu.hh"
 
+#include <algorithm>
 #include <string>
 #include <vector>
+
+#include <ncurses.h>
+#include "std_helper/string.hh"
+#include "ncurses_helper/windows.hh"
+#include "ncurses_helper/ncurses_menu_object.hh"
 
 using terminal_user_interface::ncurses_helper::win_size_t;
 using terminal_user_interface::ncurses_helper::win_coord_t;
 using terminal_user_interface::ncurses_helper::cur_coord_t;
+using terminal_user_interface::ncurses_helper::menu_option_t;
+using terminal_user_interface::ncurses_helper::windows::get_window_height;
+using terminal_user_interface::ncurses_helper::windows::get_window_width;
 
 namespace terminal_user_interface {
     namespace shell_ui {
@@ -18,19 +27,49 @@ namespace terminal_user_interface {
         }
 
         void VerticalMenu::mvwprint(cur_coord_t y, cur_coord_t x) {
-            // TODO implement
+            if (y) {}  // avoid compiler warning
+            if (x) {}  // avoid compiler warning
+
+            WINDOW *win = get_win();
+            const auto& options = get_options();
+            size_t current_option_index = 0;
+            menu_option_t selected_option = get_selected_option();
+            
+            for (register win_size_t row = 0; 
+                    row < static_cast<win_size_t>(options.size()); 
+                    ++row) {
+                wmove(win, row, 0);
+
+                const std::string& current_option = options[row];
+                win_size_t nempty_spaces = get_window_width(win) - current_option.length();
+                std::string padded_option = std_helper::string::rpad_string(
+                        std_helper::string::lpad_string(current_option,
+                            nempty_spaces / 2),
+                        nempty_spaces / 2 + current_option.length() % 2);
+
+                // highlight selected option
+                if (row == selected_option) wattron(win, A_REVERSE);
+                wprintw(win, "%s", padded_option.c_str());
+                wattroff(win, A_REVERSE);
+                ++current_option_index;  // move on to the next string
+            }
+            refresh_window();
         }
 
         win_size_t VerticalMenu::calc_height(win_size_t height, 
                 const std::vector<std::string>& options) const {
-            // TODO implement
-            return 0;
+            win_size_t min_res = options.size();
+
+            return std::max(min_res, height);
         }
 
         win_size_t VerticalMenu::calc_width(win_size_t width,
                 const std::vector<std::string>& options) const {
-            // TODO implement
-            return 0;
+            win_size_t min_res = std_helper::string::get_longest_string(options).length();
+            win_size_t result = std::max(min_res, width);
+
+            result += result % 2;  // even out the width
+            return result;
         }
     }  // namespace shell_ui
 }  // namespace terminal_user_interface
--- client/terminal-user-interface/test/shell_ui/vertical_menu_test.cc
+++ client/terminal-user-interface/test/shell_ui/vertical_menu_test.cc
@@ -2,6 +2,8 @@
 #include "__test/test_helper.hh"
 #include "shell_ui/vertical_menu.hh"
 
+#include <algorithm>
+
 #include "std_helper/string.hh"
 
 using namespace terminal_user_interface;
@@ -18,7 +20,7 @@ TEST_CASE("Display a vertical multiple-choice menu", "[shell_ui::VerticalMenu]")
     }
 
     WINDOW *win;
-    std::vector<std::string> options{"Start", "Exit", "Credits"};
+    std::vector<std::string> options{"Start", "Exit", "Credits", "Show"};
     const std::string& longest_option = std_helper::string::get_longest_string(options);
 
     SECTION("can construct object") {
@@ -30,39 +32,48 @@ TEST_CASE("Display a vertical multiple-choice menu", "[shell_ui::VerticalMenu]")
         }
         
         SECTION("fulfills the minimum width and height requirements") {
-            win_size_t height = GENERATE(-21, 0, 2);
-            win_size_t width = GENERATE(-5, 0, 5);
+            win_size_t height = GENERATE(-21, 0, 2, 7);
+            win_size_t width = GENERATE(-5, 0, 5, 12);
 
             VerticalMenu menu(options, height, width);
             win = menu.get_win();
-            REQUIRE(getmaxy(win) == options.size());
-            REQUIRE(getmaxx(win) == longest_option.length());
+            REQUIRE(getmaxy(win) >= options.size());
+            REQUIRE(getmaxx(win) >= longest_option.length());
+        }
+
+        SECTION("always evens out the width") {
+            win_size_t width = GENERATE(0, 9, 12);
+            win_size_t expected_width = std::max(width, 
+                    static_cast<win_size_t>(longest_option.length()));
+            expected_width += expected_width % 2;
+
+            VerticalMenu menu(options, 0, width);
+            win = menu.get_win();
+            REQUIRE(getmaxx(win) % 2 == 0);
+            REQUIRE(getmaxx(win) == expected_width);
         }
     }
 
     SECTION("can print a vertical menu on the screen") {
-        cur_coord_t y = GENERATE(0);
-        cur_coord_t x = GENERATE(0);
-
         SECTION("with default menu sizes") {
             VerticalMenu menu(options);
             win = menu.get_win();
-            menu.mvwprint(y, x);
-            REQUIRE(getcury(win) == y + options.size());
-            REQUIRE(getcurx(win) == x + longest_option.length());
+            menu.mvwprint();
+            REQUIRE(getcury(win) == options.size() - 1);
+            REQUIRE(getcurx(win) == longest_option.length());
         }
 
         SECTION("with custom menu sizes") {
-            win_size_t height = GENERATE(4, 7);
+            win_size_t height = GENERATE(5, 7);
             win_size_t width = GENERATE(8, 12);
             REQUIRE(height > options.size());
             REQUIRE(width > longest_option.length());
 
             VerticalMenu menu(options, height, width);
             win = menu.get_win();
-            menu.mvwprint(y, x);
-            REQUIRE(getcury(win) == y + height);
-            REQUIRE(getcurx(win) == x + width);
+            menu.mvwprint();
+            REQUIRE(getcury(win) == options.size());
+            REQUIRE(getcurx(win) == 0);
         }
     }
 
